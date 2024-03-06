4bd343f48b77669cf2f8bf87073dbf57b87f951e
Daniils Firgers <dfirger@gmail.com>
2024-03-06 21:56:05 +0200

    improved data_scraper but some bug in decode

--- data_scraping/src/lib.rs
+++ data_scraping/src/lib.rs
@@ -1,16 +1,40 @@
 pub mod logger;
+use log::error;
+use std::{sync::Arc, thread, time};
+use tokio::runtime;
+
+use lazy_static::lazy_static;
 pub mod open_meteo;
 use open_meteo::historical_data::RequestHandler as forecast_fetcher;
+
+lazy_static! {
+    static ref TOKIO_RT: Arc<runtime::Runtime> = Arc::new(
+        runtime::Builder::new_multi_thread()
+            .enable_all()
+            .worker_threads(16)
+            .build()
+            .unwrap()
+    );
+}
 
 pub fn run() {
     logger::init_logger();
 
-    let mut example = open_meteo::historical_data::RequestHandler::new(
-        "56.11".to_string(),
-        "56.11".to_string(),
-        "2024-01-01".to_string(), // Start date
-        "2024-12-31".to_string(),
+    let mut example = forecast_fetcher::new(
+        "52.52".to_string(),
+        "13.41".to_string(),
+        "2024-02-18".to_string(), // Start date
+        "2024-03-03".to_string(),
     );
-    forecast_fetcher::run(&mut example);
-    // std::thread(move || {})
+
+    TOKIO_RT.spawn(async move {
+        let result = forecast_fetcher::run(&mut example).await;
+        if let Err(e) = result {
+            error!("Error running weather scraper {:?}", e);
+        }
+    });
+
+    loop {
+        thread::sleep(time::Duration::from_millis(10000));
+    }
 }
--- data_scraping/src/open_meteo/historical_data.rs
+++ data_scraping/src/open_meteo/historical_data.rs
@@ -1,5 +1,7 @@
-use log::{info, trace};
+use log::info;
+use reqwest::Error as ReqErr;
 use serde::{Deserialize, Serialize};
+use std::error::Error;
 
 const BASE_URL: &str = "https://archive-api.open-meteo.com/v1/archive";
 const FEATURES: &str = "&hourly=temperature_2m,relative_humidity_2m,precipitation,rain,surface_pressure,wind_speed_10m,direct_radiation";
@@ -11,47 +13,36 @@ pub struct RequestHandler {
     pub end_date: String,
 }
 
-impl RequestHandler {
-    pub fn new(latitude: String, longitude: String, start_date: String, end_date: String) -> Self {
-        RequestHandler {
-            latitude,
-            longitude,
-            start_date,
-            end_date,
-        }
-    }
-}
-
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct HistoricalWeatherForecast {
     pub latitude: f64,
     pub longitude: f64,
-    #[serde(rename = "generationtime_ms")]
+    // #[serde(rename = "generationtime_ms")]
     pub generationtime_ms: f64,
-    #[serde(rename = "utc_offset_seconds")]
+    // #[serde(rename = "utc_offset_seconds")]
     pub utc_offset_seconds: i64,
     pub timezone: String,
     pub timezone_abbreviation: String,
     pub elevation: f64,
     pub hourly_units: HourlyUnits,
-    #[serde(rename = "hourly_data")]
+    // #[serde(rename = "hourly_data")]
     pub hourly: HourlyData,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct HourlyUnits {
     pub time: String,
-    #[serde(rename = "temperature")]
+    // #[serde(rename = "temperature")]
     pub temperature_2m: String,
-    #[serde(rename = "relative_humidity")]
+    // #[serde(rename = "relative_humidity")]
     pub relative_humidity_2m: String,
     pub precipitation: String,
     pub rain: String,
-    #[serde(rename = "surface_pressure")]
+    // #[serde(rename = "surface_pressure")]
     pub surface_pressure: String,
-    #[serde(rename = "wind_speed_10m")]
+    // #[serde(rename = "wind_speed_10m")]
     pub wind_speed_10m: String,
-    #[serde(rename = "direct_radiation")]
+    // #[serde(rename = "direct_radiation")]
     pub direct_radiation: String,
 }
 
@@ -70,18 +61,38 @@ pub struct HourlyData {
 }
 
 impl RequestHandler {
-    pub fn run(&mut self) {
+    pub fn new(latitude: String, longitude: String, start_date: String, end_date: String) -> Self {
+        Self {
+            latitude,
+            longitude,
+            start_date,
+            end_date,
+        }
+    }
+    pub async fn run(&mut self) -> Result<(), Box<dyn Error>> {
         info!("Historical weather forecast scraper started!");
-        self.handle_data_gathering()
+        self.handle_data_gathering().await?;
+        Ok(())
     }
 
-    fn handle_data_gathering(&mut self) {}
+    async fn handle_data_gathering(&mut self) -> Result<(), ReqErr> {
+        let url = self.create_url();
+        self.get_data(url).await?;
+        Ok(())
+    }
 
-    fn create_url(lat: &str, lon: &str, start_date: &str, end_date: &str) -> String {
+    fn create_url(&self) -> String {
         let url = format!(
             "{}?latitude={}&longitude={}&start_date={}&end_date={}&{}",
-            &BASE_URL, lat, lon, start_date, end_date, FEATURES
+            &BASE_URL, self.latitude, self.longitude, self.start_date, self.end_date, FEATURES
         );
         url
+    }
+    async fn get_data(&self, url: String) -> Result<(), ReqErr> {
+        let client = reqwest::Client::new();
+        let response: HistoricalWeatherForecast = client.get(&url).send().await?.json().await?;
+
+        println!("Deserialized data: {:?}", response);
+        Ok(())
     }
 }
