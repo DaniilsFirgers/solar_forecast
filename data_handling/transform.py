fd4b70f4861d42a869c323e3e621a9f5cd1e9184
Daniils Firgers <dfirger@gmail.com>
2024-03-23 20:54:15 +0200

    add eatly stopping and refactor ProductionDataPoint and  add WeatherDataPoint

--- data_handling/transform.py
+++ data_handling/transform.py
@@ -1,38 +1,66 @@
+import copy
 import dataclasses
 import datetime as dt
-import numpy as np
-from pandas import DataFrame
-from sklearn.preprocessing import MinMaxScaler
-from torchvision import transforms
-from torch.utils.data import Dataset
 
 
 @dataclasses.dataclass
-class Datapoint:
+class ProductionDataPoint:
+    object_name: str
     start_time: dt.datetime
-    production: float
-    interval: int
-    area: str
-    reference: str
-    internal_type: str
-    object_type: str
+    value: float
     _id: str
 
     def to_object(self):
         return {
             "start_time": self.start_time,
-            "production": self.production,
+            "value": self.value,
         }
 
 
-class CustomDataset(Dataset):
-    def __init__(self, data: DataFrame):
-        self.data = data
-        self.scaler_consumption = MinMaxScaler()
-        self.scaler_consumption.fit_transform(self.data)
+@dataclasses.dataclass
+class WeatherDataPoint:
+    object_name: str
+    start_time: dt.datetime
+    direct_radiation: float
+    precipitation: float
+    pressure: float
+    rain: float
+    relative_humidity: float
+    temperature: float
+    wind_speed: float
+    _id: str
 
-    def __len__(self):
-        return len(self.data)
+    def to_object(self):
+        return {
+            "start_time": self.start_time,
+            "direct_radiation": self.direct_radiation,
+            "precipitation": self.precipitation,
+            "pressure": self.pressure,
+            "rain": self.rain,
+            "relative_humidity": self.relative_humidity,
+            "temperature": self.temperature,
+            "wind_speed": self.wind_speed,
+        }
 
-    def __getitem__(self, idx):
-        return self.data[idx]
+
+class EarlyStopping:
+    def __init__(self, patience=10, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.best_loss = float('inf')
+        self.counter = 0
+        self.early_stop = False
+        self.best_model_weights = None
+
+    def update(self, val_loss, model):
+        if val_loss < self.best_loss - self.min_delta:
+            self.best_loss = val_loss
+            self.best_model_weights = copy.deepcopy(model.state_dict())
+            self.counter = 0
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+
+    def should_stop(self):
+        return self.early_stop
