2238eef0d68f880e524a56acc5015685c8a7b14f
Daniils Firgers <dfirger@gmail.com>
2024-03-23 20:55:07 +0200

    update main.py to use LSTM

--- main.py
+++ main.py
@@ -1,129 +1,108 @@
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from sklearn.preprocessing import MinMaxScaler
-from config.database import PRODUCTION_HISTORY_DB_NAME, PRODUCTION_HISTORY_COLLECTION_NAME
-from database.main import mongo_handler
-from data_handling.transform import CustomDataset
-import matplotlib.pyplot as plt
+import pandas as pd
 
+from sklearn.preprocessing import MinMaxScaler
+from config.database import PRODUCTION_HISTORY_DB_NAME, PRODUCTION_HISTORY_COLLECTION_NAME, PRODUCTION_WEATHER_COLLECTION_NAME
+from database.main import mongo_handler
+import matplotlib.pyplot as plt
+import matplotlib
+from sklearn.model_selection import train_test_split
+from data_handling.transform import EarlyStopping
+from models.main import LSTM
+
+plt.style.use('ggplot')
+matplotlib.use('tkagg')
 SPLIT_RATIO = 0.8
 BATCH_SIZE = 32
-NUM_EPOCHS = 100
-window = 24
+NUM_EPOCHS = 1000
+LSTM_HIDDEN_SIZE = 64
+LSTM_LAYERS = 2
+window = 1
 
 filter_query = {
-    "reference": "SADALES_TIKLS_lv_producer_43X-STJ02620895C_43Z-STO01766085R_12624502"
+    "object_name": "A"
 }
 
-historical_data = mongo_handler.retrieve_data(
+historical_data = mongo_handler.retrieve_production_data(
     PRODUCTION_HISTORY_DB_NAME, PRODUCTION_HISTORY_COLLECTION_NAME, filter_query)
-if historical_data is None:
+weather_data = mongo_handler.retrieve_weather_data(
+    PRODUCTION_HISTORY_DB_NAME, PRODUCTION_WEATHER_COLLECTION_NAME, filter_query)
+if historical_data is None or weather_data is None:
     print("Error retrieving data from MongoDB.")
     exit(1)
 
+historical_data['start_time'] = pd.to_datetime(historical_data['start_time'])
+weather_data['start_time'] = pd.to_datetime(weather_data['start_time'])
+
+merged_df = pd.merge(historical_data, weather_data,
+                     on='start_time', how='inner')
+
 scaler = MinMaxScaler()
-scaler.fit_transform(historical_data[["production"]])
+merged_df.set_index("start_time", inplace=True)
 
-# Prepare sequences for training
-X, y = [], []
-for i in range(len(historical_data) - window):
-    X.append(historical_data.iloc[i:i + window].values)
-    y.append(historical_data[i + window:i + window + 1]["production"].values)
-X = np.stack(X)
-y = np.stack(y)
+X = merged_df[['direct_radiation', 'precipitation', 'pressure',
+               'rain', 'relative_humidity', 'temperature', 'wind_speed']]
+y = merged_df['value']
 
+scaler = MinMaxScaler()
 
-# Split data into train and test sets
-train_size = int(SPLIT_RATIO * len(X))
-train_data, test_data = X[:train_size], X[train_size:]
-print(f"Train data shape: {train_data}")
-print(f"Test data shape: {test_data}")
+X_scaled = scaler.fit_transform(X)
+y_scaled = scaler.fit_transform(y.values.reshape(-1, 1))
 
-# Create instances of your custom dataset
-# train_dataset = CustomDataset(train_data)
-# test_dataset = CustomDataset(test_data)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y_scaled, test_size=0.2,  random_state=42)
 
-# Create instances of DataLoader
-train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
-test_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=False)
+X_train = torch.from_numpy(X_train).float()
+y_train = torch.squeeze(torch.from_numpy(y_train).float())
 
-model = nn.Sequential(
-    # Adjust input size based on your sequence length
-    nn.Linear(window, 64),
-    nn.ReLU(),
-    nn.Linear(64, 24)
-).float()
+X_test = torch.from_numpy(X_test).float()
+y_test = torch.squeeze(torch.from_numpy(y_test).float())
+lstm_model = LSTM(input_size=X_train.shape[1], hidden_size=LSTM_HIDDEN_SIZE,
+                  num_layers=LSTM_LAYERS, output_size=1)
 
-criterion = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-
-
-def train_one_epoch():
-    running_loss = 0.
-    last_loss = 0.
-
-    for i, inputs in enumerate(train_loader):
-        last_loss = 0.
-        inputs = inputs.float()
-        optimizer.zero_grad()
-
-        # Forward pass
-        outputs = model(inputs)
-        # Assuming you are predicting consumption values
-        targets = inputs
-        loss = criterion(outputs, targets)
-
-        # Backward pass and optimization
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-        if i % len(train_loader) == len(train_loader)-1:
-            last_loss = running_loss / len(train_loader)  # loss per batch
-            running_loss = 0.
-
-    return last_loss
+criterion = nn.MSELoss(reduction='mean')
+optimizer = torch.optim.Adam(lstm_model.parameters(), lr=0.001)
 
 
 train_losses = []
-val_losses = []
+test_losses = []
 
 
-BEST_VAL_LOSS = 1_000_000_000
+early_stopping = EarlyStopping(patience=50, min_delta=0.0001)
+
+BEST_VAL_LOSS = int(1e9)
 # Training loop
 for epoch in range(NUM_EPOCHS):
-    model.train()
-    # Train for one epoch
-    train_loss = train_one_epoch()
+    outputs = lstm_model(X_train.unsqueeze(1)).squeeze()
+    optimizer.zero_grad()
+    loss = criterion(outputs, y_train)
+    loss.backward()
+    optimizer.step()
+    train_losses.append(loss.item())
 
     running_vloss = 0.0
-    model.eval()
+
     with torch.no_grad():
-        for i, v_data in enumerate(test_loader):
-            v_data = v_data.float()
-            v_outputs = model(v_data)
-            v_targets = v_data
-            v_loss = criterion(v_outputs, v_targets)
-            running_vloss += v_loss.item()
+        test_outputs = lstm_model(X_test.unsqueeze(1)).squeeze()
+        test_loss = criterion(test_outputs, y_test)
 
-    val_loss = running_vloss / (i + 1)
-    print('Epoch {} - LOSS train {} valid {}'.format(epoch, train_loss, val_loss))
+    # val_loss = running_vloss / len(test_loader)
+    if (epoch + 1) % 10 == 0:
+        print(
+            f"Epoch [{epoch+1}/{NUM_EPOCHS}], Train Loss: {loss.item():.4f}, Test Loss: {test_loss.item():.4f}")
 
-    train_losses.append(train_loss)
-    val_losses.append(val_loss)
-
-    if val_loss < BEST_VAL_LOSS:
-        BEST_VAL_LOSS = val_loss
-        torch.save(model.state_dict(), 'trained_models/model.pt')
+    test_losses.append(test_loss)
+    early_stopping.update(test_loss, lstm_model)
+    if early_stopping.should_stop():
+        print(f'Early stopping at epoch {epoch}')
+        break
 
 plt.figure(figsize=(10, 6))
-plt.plot(range(1, NUM_EPOCHS + 1), train_losses, label='Training Loss')
-plt.plot(range(1, NUM_EPOCHS + 1), val_losses, label='Validation Loss')
+plt.plot(range(1, len(train_losses) + 1), train_losses, label='Training Loss')
+plt.plot(range(1, len(train_losses) + 1), test_losses, label='Validation Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
-plt.title('Training and Validation Loss Over Epochs')
+plt.title('Training and Validation Loss for LSTM Model')
 plt.legend()
 plt.show()
