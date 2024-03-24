3b676f8ede1eb0baa4ca64ff571e74a5097abd05
Daniils Firgers <dfirger@gmail.com>
2024-03-24 10:41:45 +0200

    unused dropout

--- models/main.py
+++ models/main.py
@@ -4,12 +4,12 @@ import torch
 
 
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, dropout=0.2):
         super(LSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size,
-                            num_layers, batch_first=True)
+                            num_layers, dropout=dropout, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
