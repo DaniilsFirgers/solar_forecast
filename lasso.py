e2f9122e4d887051b455c083d26fbffb036f61f0
Daniils Firgers <dfirger@gmail.com>
2024-04-15 10:44:30 +0300

    updaet lasso algortihm

--- lasso.py
+++ lasso.py
@@ -1,27 +1,19 @@
+import numpy as np
 from sklearn.linear_model import Lasso
-import torch
-import torch.nn as nn
-import pandas as pd
-
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
 from config.database import FORECAST_DB_NAME, PRODUCTION_COLLECTION_NAME, WEATHER_COLLECTION_NAME
 from database.main import mongo_handler
 import matplotlib.pyplot as plt
 import matplotlib
 from sklearn.model_selection import train_test_split
-from data_handling.transform import EarlyStopping, Plot, DataTransformer
-from models.main import LSTM
-from sklearn.metrics import mean_squared_error, r2_score
+from data_handling.transform import PlotPredictions, DataTransformer
 
 plt.style.use('ggplot')
 matplotlib.use('tkagg')
-SPLIT_RATIO = 0.75
-NUM_EPOCHS = 1000
-LSTM_HIDDEN_SIZE = 128
-LSTM_LAYERS = 1
-OBJECTS = ['B']
-INPUT_FEATURES = ['temperature', 'relative_humidity', 'pressure', 'rain',
-                  'wind_speed', "shortwave_radiation", 'month', 'day_of_week', 'hour']
+SPLIT_RATIO = 0.70
+OBJECTS = ['A']
+INPUT_FEATURES = ['direct_radiation', 'value_lag_1']
 LAGGED_FEATURES = ['value']
 LAG_STEPS = 1
 
@@ -44,7 +36,7 @@ for object in OBJECTS:
     data_transformer = DataTransformer(historical_data, weather_data)
 
     merged_df = data_transformer.get_merged_df()
-    # data_transformer.add_lagged_features(LAGGED_FEATURES, LAG_STEPS)
+    data_transformer.add_lagged_features(LAGGED_FEATURES, LAG_STEPS)
     X = merged_df[INPUT_FEATURES]
     y = merged_df['value']
 
@@ -56,37 +48,38 @@ for object in OBJECTS:
 
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(
-        X_scaled, y_scaled, test_size=SPLIT_RATIO, random_state=42)
+        X_scaled, y_scaled, test_size=SPLIT_RATIO, random_state=17)
 
+    split_index = int(len(X_scaled) * (1 - SPLIT_RATIO))
+    ground_truth_df = merged_df['value'].iloc[split_index:]
+
+    results_plot_title = f'Ražošanas prognozes pret patiesajām vērtībām Lasso modelim {object} objektam'
+    results_save_path = f'plots/lasso-{object}-results.png'
     # Define the Lasso regression model
-    lasso_model = Lasso(alpha=0.001)  # Adjust alpha as needed
+    lasso_model = Lasso(alpha=0.01)  # Adjust alpha as needed
 
     # Train the Lasso model
     lasso_model.fit(X_train, y_train)
+    train_score_ls = lasso_model.score(X_train, y_train)
+    test_score_ls = lasso_model.score(X_test, y_test)
 
     # Make predictions
     y_train_pred = lasso_model.predict(X_train)
-    y_test_pred = lasso_model.predict(X_test)
+    predictions = lasso_model.predict(X_test)
 
-    # Reverse scaling for predictions
-    y_train_pred_original = y_scaler.inverse_transform(
-        y_train_pred.reshape(-1, 1))
-    y_test_pred_original = y_scaler.inverse_transform(
-        y_test_pred.reshape(-1, 1))
-    y_train_original = y_scaler.inverse_transform(
-        y_train.reshape(-1, 1))
-    y_test_original = y_scaler.inverse_transform(
+    mse = mean_squared_error(y_test, predictions)
+
+    predictions = y_scaler.inverse_transform(
+        predictions.reshape(-1, 1))
+    ground_truth = y_scaler.inverse_transform(
         y_test.reshape(-1, 1))
 
-    # Evaluate the model
-    train_rmse = mean_squared_error(
-        y_train_original, y_train_pred_original, squared=False)
-    test_rmse = mean_squared_error(
-        y_test_original, y_test_pred_original, squared=False)
-    train_r2 = r2_score(y_train_original, y_train_pred_original)
-    test_r2 = r2_score(y_test_original, y_test_pred_original)
+    results_plot = PlotPredictions('Lasso', object_name=object, title=results_plot_title, save_path=results_save_path, ground_truth=ground_truth_df,
+                                   x_data=ground_truth, y_data=predictions)
 
-    print("Train RMSE:", train_rmse)
-    print("Test RMSE:", test_rmse)
-    print("Train R^2:", train_r2)
-    print("Test R^2:", test_r2)
+    mse = mean_squared_error(ground_truth, predictions)
+
+    print("Test R^2:", test_score_ls)
+    print("Train R^2:", train_score_ls)
+    print("Mean Squared Error:", np.sqrt(mse))
+    results_plot.create_plot()
