a6567f381167b42d0286e00210453baa82e7872e
Daniils Firgers <dfirger@gmail.com>
2024-04-21 21:15:34 +0300

    add GRU model

--- models/main.py
+++ models/main.py
@@ -63,3 +63,26 @@ class RNN(nn.Module):
         out = F.softplus(out, beta=35, threshold=1)
         # out.size() --> 100, 10
         return out
+
+
+class GRU(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=1):
+        super(GRU, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        h0 = torch.zeros(self.num_layers, x.size(0),
+                         self.hidden_dim).to(x.device)
+
+        # Forward propagate GRU
+        # out: tensor of shape (batch_size, seq_length, hidden_dim)
+        out, _ = self.gru(x, h0)
+
+        # Decode the hidden state of the last time step
+        out = self.fc(out[:, -1, :])
+        out = F.softplus(out, beta=35, threshold=1)
+        return out
