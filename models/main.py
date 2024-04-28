24541e0edeb8a5e859243ea8c51107cfb2552cd6
Daniils Firgers <dfirger@gmail.com>
2024-04-28 20:26:14 +0300

    update models

--- models/main.py
+++ models/main.py
@@ -4,10 +4,11 @@ import torch
 
 
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, dropout=0):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, negative_slope, dropout=0):
         super(LSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        self.negative_slope = negative_slope
 
         self.h0 = nn.Parameter(torch.zeros(num_layers, 1, hidden_size))
         self.c0 = nn.Parameter(torch.zeros(num_layers, 1, hidden_size))
@@ -23,23 +24,19 @@ class LSTM(nn.Module):
         out, _ = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
 
-        out = F.softplus(out, beta=35, threshold=1)
+        out = F.leaky_relu(out, self.negative_slope)
         return out
 
 
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, dropout=0):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, negative_slope, dropout=0):
         super(RNN, self).__init__()
         # Hidden dimensions
         self.hidden_dim = hidden_size
+        self.negative_slope = negative_slope
 
         # Number of hidden layers
         self.num_layers = num_layers
-
-        # Building your RNN
-        # batch_first=True causes input/output tensors to be of shape
-        # (batch_dim, seq_dim, input_dim)
-        # batch_dim = number of samples per batch
         self.rnn = nn.RNN(input_size, hidden_size, num_layers,
                           batch_first=True, dropout=dropout)
 
@@ -47,8 +44,6 @@ class RNN(nn.Module):
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        # Initialize hidden state with zeros
-        # (layer_dim, batch_size, hidden_dim)
         h0 = torch.zeros(self.num_layers, x.size(
             0), self.hidden_dim).requires_grad_()
 
@@ -56,11 +51,8 @@ class RNN(nn.Module):
         # This is part of truncated backpropagation through time (BPTT)
         out, hn = self.rnn(x, h0.detach())
 
-        # Index hidden state of last time step
-        # out.size() --> 100, 28, 10
-        # out[:, -1, :] --> 100, 10 --> just want last time step hidden states!
         out = self.fc(out[:, -1, :])
-        out = F.softplus(out, beta=35, threshold=1)
+        out = F.leaky_relu(out, self.negative_slope)
         # out.size() --> 100, 10
         return out
 
